9303eca5f495dde89edcd7a44acd964d0e34a146
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2015-12-19 20:20:47 +0100

    BACK: MIPS: do not build position-independent executables for SPL
    SPL binaries are usually linked to a fixed address in SRAM.
    Furthermore SPL binaries do not need to relocate itself. Thus
    do not build them as position-independent binaries which helps
    to largely reduce the size of SPL binaries.
    
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

--- arch/mips/config.mk
+++ arch/mips/config.mk
@@ -45,9 +45,21 @@ PLATFORM_CPPFLAGS += -DCONFIG_MIPS -D__MIPS__
 # On the other hand, we want PIC in the U-Boot code to relocate it from ROM
 # to RAM. $28 is always used as gp.
 #
-PLATFORM_CPPFLAGS		+= -G 0 -mabicalls -fpic $(ENDIANNESS)
+
+ifdef CONFIG_SPL_BUILD
+PF_ABICALLS			:= -mno-abicalls
+PF_PIC				:= -fno-pic
+PF_PIE				:=
+else
+PF_ABICALLS			:= -mabicalls
+PF_PIC				:= -fpic
+PF_PIE				:= -pie
+PF_OBJCOPY			:= -j .got -j .u_boot_list -j .rel.dyn -j .padding
+endif
+
+PLATFORM_CPPFLAGS		+= -G 0 $(PF_ABICALLS) $(PF_PIC) $(ENDIANNESS)
 PLATFORM_CPPFLAGS		+= -msoft-float
 PLATFORM_LDFLAGS		+= -G 0 -static -n -nostdlib $(ENDIANNESS)
 PLATFORM_RELFLAGS		+= -ffunction-sections -fdata-sections
-LDFLAGS_FINAL			+= --gc-sections -pie
-OBJCFLAGS			+= -j .u_boot_list -j .rel.dyn -j .padding
+LDFLAGS_FINAL			+= --gc-sections $(PF_PIE)
+OBJCFLAGS			+= -j .text -j .rodata -j .data $(PF_OBJCOPY)
