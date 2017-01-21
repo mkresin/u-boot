3aee6f7d3fcdd2dea7407cb074594264dedbc9cb
Mathias Kresin <dev@kresin.me>
2017-01-21 11:09:34 +0100

    TEST: MIPS: add lzma compressed spl to Arcadyan ARV752DPW22

--- include/configs/arv752dpw22.h
+++ include/configs/arv752dpw22.h
@@ -18,6 +18,15 @@
 
 #define CONFIG_LTQ_SUPPORT_NOR_FLASH	/* Have a parallel NOR flash */
 
+#define CONFIG_LTQ_SUPPORT_SPL_NOR_FLASH	/* Build NOR flash SPL */
+#define CONFIG_LTQ_SPL_CONSOLE
+#define CONFIG_LTQ_SPL_COMP_LZMA
+#define CONFIG_SPL_TPL_OFFS		0x800
+#define CONFIG_SPL_TPL_SIZE		0x5000
+#define CONFIG_SPL_MC_TUNE_OFFS		0x5800
+#define CONFIG_SPL_U_BOOT_OFFS		0x6000
+#define CONFIG_SPL_U_BOOT_SIZE		0x32000
+
 /* support (uncompressed) kernels up to 32 MByte */
 #define CONFIG_SYS_LOAD_ADDR		0x82000000
 #define CONFIG_SYS_BOOTM_LEN		0x2000000
@@ -33,7 +42,7 @@
 #define CONFIG_SWITCH_AR8216
 
 /* Environment */
-#if defined(CONFIG_SYS_BOOT_NOR)
+#if defined(CONFIG_SYS_BOOT_NOR) || defined(CONFIG_SYS_BOOT_NORSPL)
 #define CONFIG_ENV_IS_IN_FLASH
 #define CONFIG_ENV_OVERWRITE
 #define CONFIG_ENV_OFFSET		(192 * 1024)
@@ -57,7 +66,7 @@
 #include "openwrt-lantiq-common.h"
 
 #define CONFIG_ENV_UPDATE_UBOOT_NOR		\
-	"update-uboot-nor=run load-uboot-nor write-uboot-nor\0"
+	"update-uboot-nor=run load-uboot-norspl-lzma write-uboot-nor\0"
 
 #define CONFIG_EXTRA_ENV_SETTINGS	\
 	CONFIG_ENV_LANTIQ_DEFAULTS	\
