d7603b310c2f57bee21cf6683e51af40d597d421
Mathias Kresin <dev@kresin.me>
2021-11-17 00:14:19 +0100

    build: fix compiler warnings
    Signed-off-by: Mathias Kresin <dev@kresin.me>

--- disk/part.c
+++ disk/part.c
@@ -19,6 +19,7 @@
 #define PRINTF(fmt,args...)
 #endif
 
+#ifdef HAVE_BLOCK_DEVICE
 struct block_drvr {
 	char *name;
 	block_dev_desc_t* (*get_dev)(int dev);
@@ -53,6 +54,7 @@ static const struct block_drvr block_drvr[] = {
 #endif
 	{ },
 };
+#endif
 
 DECLARE_GLOBAL_DATA_PTR;
 
